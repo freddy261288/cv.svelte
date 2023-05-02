c13c10b51094bd31fa571bdcbb3c7c0949e21fff
freddy261288 <115787456+freddy261288@users.noreply.github.com>
2023-05-02 08:04:59 +0000

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,11 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Peralta&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d0c488930b.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</svelte:head>
+
 <div class="parent">
 <div class="sidebar"/>
 <div class="content">
