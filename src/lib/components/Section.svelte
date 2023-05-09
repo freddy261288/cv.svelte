d59fb5f9774e463cd1b820d64c419f54db52a9f6
freddy261288 <115787456+freddy261288@users.noreply.github.com>
2023-05-09 08:33:21 +0000

    use grid(unfinished)

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import Icon from "svelte-awesome";
+    import type { IconType } from "svelte-awesome/components/Icon.svelte";
+
+    export let iconData:IconType;
+    export let title: string;
+</script>
+
+<div.class="container">
+<div class="section-title">
+    <Icon data={user} />
+    {title}
+</div>
+<slot/>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+    .section-title {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 15px;
+		background-color: #ffeb3b;
+		border-left: 4px solid #fbc02d;
+		color: black;
+		padding: 8px 16px;
+		text-transform: UPPERCASE;
+		font-weight: 500;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import Icon from 'svelte-awesome';
-
 	import book from 'svelte-awesome/icons/book';
 	import user from 'svelte-awesome/icons/user';
 	import briefcase from 'svelte-awesome/icons/briefcase';
 	import cogs from 'svelte-awesome/icons/cogs';
+
+    import Section from '$components/Section.svelte';
 </script>
 
 <div class="readable-area">
@@ -19,9 +20,86 @@
 			src="https://play-lh.googleusercontent.com/9XA4q4TKYtF2l5oN10M8oumPauR9g5lFOAJXjczrd3Gmq9cwQ8QOQJ-2KZv84s1oTA"
 		/>
 	</a>
-
 	<div class="content">
 		<div class="content-column">
+			<Section iconData={user} title="Profile"> 
+				<p>
+					Hello, my name is Spongebob Squarepants and I'm passionate about creating the best possible
+					food. In my spare time, I like to catch jellyfishes with my trusty net.
+				</p>
+			</Section>
+			<Section iconData={book} title="Education">
+				<ul>
+					<li>
+						2011-2014
+						<br />
+						&emsp; Bikini Bottom High School
+					</li>
+					<li>
+						2014-2018
+						<br />
+						&emsp; New Kelp City Culinary College
+					</li>
+				</ul>
+			</Section>
+		</div>
+		<div class ="section-title">
+			<Section iconData={briefcase} title="Work History"> 
+				<ul>
+					<li>
+						Head Cook (2022 - ongoing)
+						<br />
+						&emsp; Krusty Krab, Inc.
+					</li>
+					<li>
+						Marketing Associate (2020 - 2022)
+						<br />
+						&emsp; Chum Bucket Ltd.
+					</li>
+					<li>
+						Jellyfish Catcher (2018 - 2020)
+						<br />
+						&emsp; Freelance
+					</li>
+				</ul>
+			</Section>
+		</div>
+		    <div class="section-title">
+			<Section iconData={cogs} title="Skills">
+				<ul>
+					<li>
+						<span>Jellyfish Hunting</span>
+						<div class="gauge">
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+						</div>
+					</li>
+					<li>
+						<span>English</span>
+						<div class="gauge">
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet" />
+						</div>
+					</li>
+					<li>
+						<span>Whale Singing</span>
+						<div class="gauge">
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet filled-bullet" />
+							<div class="bullet" />
+							<div class="bullet" />
+						</div>
+					</li>
+				</ul>
+			</Section>
+		</div>
 			<div class="section-title">
 				<Icon data={user} />
 				Profile
@@ -182,8 +260,10 @@
 	}
 
 	.content {
-		display: flex;
-		flex-direction: row;
+		display: grid;
+		grid-template-columns: repeat (2, minmax(0, 1fr));
+		gap: 32px;
+		padding: 32px;
 	}
 
 	.content-column {
@@ -224,7 +304,7 @@
 		}
 
 		.content {
-			flex-direction: column;
+		  grid-template-columns: repeat(1, minmax(0, 1fr));
 		}
 	}
 </style>
